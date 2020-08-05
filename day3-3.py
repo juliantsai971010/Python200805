2bfe3397ed2c99f5b7985f23275e3056f3e980fb
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-05 11:43:19 +0800

    Add files via upload

---
+++ day3-3.py
@@ -0,0 +1,55 @@
+A=[]
+B=[]
+total=0
+c=input('how many people is there')
+c=int(c)
+d=int(d)
+for d in range(c):
+        a=input('enter the score')
+        a=int(a)
+        A.append(a)
+        total=total+a
+for d in range(c):
+        b=input('enter the name')
+        B.append(b)
+
+def i (total,c):        
+    for d in range(c):
+            return(total/c)
+           
+print('總分是',total)
+print('平均是',i (total,c))
+i(total,c)
+low=0
+def j (A,B,c):
+    high=0
+    for d in range(c):
+        if A[d]>high:
+            A[d]=high
+        return(high)
+print('最高分是',j(A,B,c))
+
+j(A,B,c)
+def k (A,B,c):
+    low=0
+    for d in range(c):
+        if A[d]>low:
+            A[d]=low
+        return(low)
+print('最低分是',k(A,B,c))
+
+k(A,B,c)   
+   
+
+
+      
+   
+
+
+    
+    
+    
+    
+
+#
+#    
