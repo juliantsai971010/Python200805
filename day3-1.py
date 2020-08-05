d1cef9eceb8e93e9fb226d964a43d814fda34420
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-05 09:39:03 +0800

    Add files via upload

---
+++ day3-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 09:22:55 2020
+
+@author: USER
+"""
+
+for a in range(1,10):
+    for b in range(1,10):
+        print(a,'*',b,'=',a*b)
+        
+    print( )
